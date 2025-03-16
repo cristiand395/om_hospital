771510360cee926b2a6ae435315f5fd7c8437681
Cristian Davila <cristiand395@gmail.com>
2025-03-16 19:19:16 +0100

    Add tracking to fields

--- models/patient.py
+++ models/patient.py
@@ -6,10 +6,12 @@ class HospitalPatient(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "Hospital Patient"
 
-    name = fields.Char(string="Name")
-    last_name = fields.Char(string="Last Name")
-    age = fields.Integer(string="Age")
+    name = fields.Char(string="Name", tracking=True)
+    last_name = fields.Char(string="Last Name", tracking=True)
+    age = fields.Integer(string="Age", tracking=True)
     gender = fields.Selection(
-        [("male", "Male"), ("female", "Female")], string="Gender"
+        [("male", "Male"), ("female", "Female")],
+        string="Gender",
+        tracking=True,
     )
-    active = fields.Boolean(string="Active", default=True)
+    active = fields.Boolean(string="Active", default=True, tracking=True)
