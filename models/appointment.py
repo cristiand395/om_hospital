d9825a8292e98820683bf70360eed3aca4849907
Cristian Davila <cristiand395@gmail.com>
2025-03-17 21:14:36 +0100

    Add related field

--- models/appointment.py
+++ models/appointment.py
@@ -10,6 +10,9 @@ class HospitalAppointment(models.Model):
     patient_id = fields.Many2one(
         comodel_name="hospital.patient", string="Patient"
     )
+    gender = fields.Selection(
+        string="Patient Gender", related="patient_id.gender"
+    )
     appointment_time = fields.Datetime(
         string="Appointment Time", default=fields.Datetime.now
     )
